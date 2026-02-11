866b936d7ea766fa8f189f2497ce29a3d70be7dd
AnyaPizza <axolotlanya18@gmail.com>
2026-02-11 16:59:48 -0500

    Updated to Snapshot 7

--- src/main/java/anya/pizza/houseki/HousekiClient.java
+++ src/main/java/anya/pizza/houseki/HousekiClient.java
@@ -1,21 +1,13 @@
 package anya.pizza.houseki;
 
-import anya.pizza.houseki.block.ModBlocks;
 import anya.pizza.houseki.screen.ModScreenHandlers;
 import anya.pizza.houseki.screen.custom.CrusherScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.ChunkSectionLayerMap;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 public class HousekiClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
-        ChunkSectionLayerMap.putBlock(ModBlocks.ALUMINUM_GLASS, ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(ModBlocks.ALUMINUM_GLASS_PANE, ChunkSectionLayer.TRANSLUCENT);
-        ChunkSectionLayerMap.putBlock(ModBlocks.ALUMINUM_DOOR, ChunkSectionLayer.CUTOUT);
-        ChunkSectionLayerMap.putBlock(ModBlocks.ALUMINUM_TRAPDOOR, ChunkSectionLayer.CUTOUT);
 
         MenuScreens.register(ModScreenHandlers.CRUSHER_SCREEN_HANDLER, CrusherScreen::new);
 
